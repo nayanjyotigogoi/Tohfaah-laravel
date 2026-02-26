fda51a742d62411ffa690c85345bb7f2e278e92a
Panchal Khargharia <panchal.pk123@gmail.com>
2026-02-26 23:55:44 +0530

    fix: email to invite to memory map

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    
+
 
     /*
     |--------------------------------------------------------------------------
@@ -56,6 +56,8 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
     'asset_url' => env('ASSET_URL', null),
 
